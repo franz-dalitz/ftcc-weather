55e162549d2d66cc5dfa2713e26acd725e11306a
Franz Dalitz <80651545+Franz-Dalitz@users.noreply.github.com>
2024-06-17 10:22:25 +0200

    some final script changes

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm frontend.log
+rm backend.log
 screen -L -Logfile frontend.log -dSm frontend npm run --prefix frontend start
 screen -L -Logfile backend.log -dSm backend npm run --prefix backend start
